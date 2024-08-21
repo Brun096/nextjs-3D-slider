0a596d3fb1039b6a3eeddd40be63f916e58056e7
Bruno Huerta <brunohuerta_08@hotmail.com>
2024-08-21 21:37:27 +0200

    chore: Update npm dependencies and add video slider component

---
+++ src/components/slider/videos.js
@@ -0,0 +1,26 @@
+export const videos = [
+  {
+    id: "711863471",
+    title: "Video 1",
+    category: "Category 1",
+    date: "May 2022"
+  },
+  {
+    id: "711863471",
+    title: "Video 1",
+    category: "Category 1",
+    date: "May 2022"
+  },
+  {
+    id: "711863471",
+    title: "Video 1",
+    category: "Category 1",
+    date: "May 2022"
+  },
+  {
+    id: "711863471",
+    title: "Video 1",
+    category: "Category 1",
+    date: "May 2022"
+  },
+]
