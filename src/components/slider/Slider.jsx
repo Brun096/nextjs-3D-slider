a5ab8d531472c9bccf4a296297ac78b4bb025581
Bruno Huerta <brunohuerta_08@hotmail.com>
2024-08-21 22:19:10 +0200

    chore: Add video slider functionality to Slider component

--- src/components/slider/Slider.jsx
+++ src/components/slider/Slider.jsx
@@ -1,27 +1,49 @@
 "use client";
 
 import React, { useState, useEffect, useRef } from 'react';
-import gsap from 'gsap';
+import gsap from 'gsap';    
+import { Observer } from "gsap/Observer";
+
+
+gsap.registerPlugin(Observer);
 import dynamic from 'next/dynamic';
 import { videos } from './videos';
 
 const ReacPlayer = dynamic(() => import('react-player'), { ssr: false });
 
+
 const Slider = () => {
   const sliderRef = useRef(null);
   const [isAnimating, setIsAnimating] = useState(false);
   const [isClient, setIsClient] = useState(false);
+  let animating = false;
+ 
 
   useEffect(() => {
     setIsClient(true);
   }, []);
 
   useEffect(() => {
-    if(isClient && sliderRef.current) {
+    const slider = sliderRef.current;
+    if(isClient && slider) {
       initializeCards();
     }
   }
   , [isClient]);
+
+  Observer.create({
+      target: sliderRef.current, // can be any element (selector text is fine)
+      type: "wheel,touch", // comma-delimited list of what to listen for ("wheel,touch,scroll,pointer")
+      wheelSpeed: -1,
+      onUp: () => !animating && slideToUp(),
+      onDown: () => {
+        !animating && slideToDown()
+      },
+      onStop: () => animating = false,
+      tolerance: 10,
+      preventDefault: true,
+      scrollSpeed: 0.01
+    });
 
   const initializeCards = () => {
     const cards = Array.from(sliderRef.current.querySelectorAll('.card'));
@@ -34,14 +56,11 @@ const Slider = () => {
     })
   };
 
-  const handleClick = () => {
-    if(isAnimating) return;
-    setIsAnimating(true);
-
+  const slideToDown = () => {
     const slider = sliderRef.current;
     const cards = Array.from(slider.querySelectorAll('.card'));
     const lastCard = cards.pop();
-
+    animating = true;
     gsap.to(lastCard, {
       y: "+=150%",
       duration: 0.75,
@@ -50,9 +69,30 @@ const Slider = () => {
         setTimeout(() => {
           slider.prepend(lastCard);
           initializeCards();
-          setTimeout(() => {
-            setIsAnimating(false);
-          }, 1000);
+          /* setTimeout(() => {
+            
+          }, 1000); */
+        }, 300);
+      }
+    })
+  }
+
+  const slideToUp = () => {
+    const slider = sliderRef.current;
+    const cards = Array.from(slider.querySelectorAll('.card'));
+    const firstCard = cards.shift();
+    animating = true;
+    gsap.to(firstCard, {
+      y: "+=150%",
+      duration: 0.75,
+      ease: 'power3.inOut',
+      onStart: () => {
+        setTimeout(() => {
+          slider.append(firstCard);
+          initializeCards();
+          /* setTimeout(() => {
+            
+          }, 1000); */
         }, 300);
       }
     })
@@ -60,7 +100,7 @@ const Slider = () => {
 
   return (
     <>
-      <div className="container" onClick={handleClick}>
+      <div className="container">
         <div className="slider" ref={sliderRef}>
           {videos.map((video, index) => (
             <div className="card" key={index}>
